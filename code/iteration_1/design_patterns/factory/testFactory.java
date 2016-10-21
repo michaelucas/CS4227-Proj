2819c5de0ab98ecce1281c9b3aacbbe11dc57b30
michaelucas <13112732@studentmail.ul.ie>
2016-10-21 16:54:31 +0100

    Update testFactory.java

--- code/iteration_1/design_patterns/factory/testFactory.java
+++ code/iteration_1/design_patterns/factory/testFactory.java
@@ -1,14 +1,15 @@
-package compositeSoFar;
+
 import java.io.*;
 import java.util.*;
 
 public class testFactory {
 
-	public static void main(String[] args) throws IOException {
+	public static ArrayList<Component> getComponentList() throws IOException {
 		
 		
 		File componentFile = new File("ComponentList.txt");
 		FactoryDesignPattern component = new FactoryDesignPattern();
+		ArrayList<Component> componentList = new ArrayList<Component>();
 		Scanner reader = new Scanner(componentFile);
 		while(reader.hasNextLine()){
 			String LineFromFile = reader.nextLine();
@@ -16,40 +17,38 @@ public class testFactory {
 			switch(pieceOfLine[3]){
 			case "CPU":
 				CPU cpuComponent = component.getCPU(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], pieceOfLine[4], pieceOfLine[5]);
+				componentList.add(cpuComponent);
 				break;
 			case "GPU":
 				GPU gpuComponent = component.getGPU(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], pieceOfLine[4], Integer.parseInt(pieceOfLine[5]));
+				componentList.add(gpuComponent);
 				break;
 			case "MemoryDrive":
 				MemoryDrive memoryComponent = component.getMemoryDrive(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], Integer.parseInt(pieceOfLine[4]), Boolean.parseBoolean(pieceOfLine[5]));
+				componentList.add(memoryComponent);
 				break;
 			case "Monitor":
 				Monitor monitorComponent = component.getMonitor(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], pieceOfLine[4], Boolean.parseBoolean(pieceOfLine[5]), Boolean.parseBoolean(pieceOfLine[6]));
+				componentList.add(monitorComponent);
 				break;
 			case "Motherboard":
 				Motherboard motherboardComponent = component.getMotherboard(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], pieceOfLine[4], pieceOfLine[5], pieceOfLine[6]);
+				componentList.add(motherboardComponent);
 				break;
 			case "Mouse":
 				Mouse mouseComponent = component.getMouse(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], Integer.parseInt(pieceOfLine[4]), Boolean.parseBoolean(pieceOfLine[5]), Boolean.parseBoolean(pieceOfLine[6]));
+				componentList.add(mouseComponent);
 				break;
 			case "RAM":
 				RAM ramComponent = component.getRAM(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], Integer.parseInt(pieceOfLine[4]), pieceOfLine[5]);
-				break;
-			default:
-				System.out.println("Houston we have a problem");
-				break;		
+				componentList.add(ramComponent);
+				break;	
 			}
 			
 		}
 		reader.close();
-				
-		for(int i = 0; i < componentList.size(); i++){
-			Component c = componentList.get(i);
-			System.out.println(c.getComponentDetails());
-		}
-		
-		
-		System.out.println("SUo");
+
+		return componentList;
 
 	}
 
