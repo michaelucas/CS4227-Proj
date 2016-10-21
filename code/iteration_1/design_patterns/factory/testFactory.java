cf435cbdcc2cae48c745ddc12eb700dfd79bb7dd
michaelucas <13112732@studentmail.ul.ie>
2016-10-21 18:11:14 +0100

    Update testFactory.java

--- code/iteration_1/design_patterns/factory/testFactory.java
+++ code/iteration_1/design_patterns/factory/testFactory.java
@@ -1,6 +1,8 @@
-
+package Factory;
 import java.io.*;
 import java.util.*;
+
+import Component.*;
 
 public class testFactory {
 
@@ -14,35 +16,42 @@ public class testFactory {
 		while(reader.hasNextLine()){
 			String LineFromFile = reader.nextLine();
 			String[] pieceOfLine = LineFromFile.split(",");
-			switch(pieceOfLine[3]){
+			switch(pieceOfLine[4]){
 			case "CPU":
-				CPU cpuComponent = component.getCPU(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], pieceOfLine[4], pieceOfLine[5]);
+				CPU cpuComponent = component.getCPU(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], pieceOfLine[5], 
+						pieceOfLine[6]);
 				componentList.add(cpuComponent);
 				break;
 			case "GPU":
-				GPU gpuComponent = component.getGPU(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], pieceOfLine[4], Integer.parseInt(pieceOfLine[5]));
+				GPU gpuComponent = component.getGPU(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]),Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], pieceOfLine[5], 
+						Integer.parseInt(pieceOfLine[6]));
 				componentList.add(gpuComponent);
 				break;
 			case "MemoryDrive":
-				MemoryDrive memoryComponent = component.getMemoryDrive(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], Integer.parseInt(pieceOfLine[4]), Boolean.parseBoolean(pieceOfLine[5]));
+				MemoryDrive memoryComponent = component.getMemoryDrive(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], Integer.parseInt(pieceOfLine[5]), 
+						Boolean.parseBoolean(pieceOfLine[6]));
 				componentList.add(memoryComponent);
 				break;
 			case "Monitor":
-				Monitor monitorComponent = component.getMonitor(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], pieceOfLine[4], Boolean.parseBoolean(pieceOfLine[5]), Boolean.parseBoolean(pieceOfLine[6]));
+				Monitor monitorComponent = component.getMonitor(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], pieceOfLine[5], 
+						Boolean.parseBoolean(pieceOfLine[6]), Boolean.parseBoolean(pieceOfLine[7]));
 				componentList.add(monitorComponent);
 				break;
 			case "Motherboard":
-				Motherboard motherboardComponent = component.getMotherboard(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], pieceOfLine[4], pieceOfLine[5], pieceOfLine[6]);
+				Motherboard motherboardComponent = component.getMotherboard(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], pieceOfLine[5],
+						pieceOfLine[6], pieceOfLine[7]);
 				componentList.add(motherboardComponent);
 				break;
 			case "Mouse":
-				Mouse mouseComponent = component.getMouse(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], Integer.parseInt(pieceOfLine[4]), Boolean.parseBoolean(pieceOfLine[5]), Boolean.parseBoolean(pieceOfLine[6]));
+				Mouse mouseComponent = component.getMouse(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], Integer.parseInt(pieceOfLine[5]), 
+						Boolean.parseBoolean(pieceOfLine[6]), Boolean.parseBoolean(pieceOfLine[7]));
 				componentList.add(mouseComponent);
 				break;
 			case "RAM":
-				RAM ramComponent = component.getRAM(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), pieceOfLine[3], Integer.parseInt(pieceOfLine[4]), pieceOfLine[5]);
+				RAM ramComponent = component.getRAM(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], Integer.parseInt(pieceOfLine[5]), 
+						pieceOfLine[6]);
 				componentList.add(ramComponent);
-				break;	
+				break;
 			}
 			
 		}
