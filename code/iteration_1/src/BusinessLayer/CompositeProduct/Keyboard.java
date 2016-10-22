45ea755e3c64bc594d5cb73950661a72e4e867bc
conorjduggan-ul <13127004@studentmail.ul.ie>
2016-10-22 20:54:09 +0100

    Adding rest of composite stuff

---
+++ code/iteration_1/src/BusinessLayer/CompositeProduct/Keyboard.java
@@ -0,0 +1,34 @@
+package Component;
+
+import Component.Component;
+
+public class Keyboard extends Component {
+    private String keySwitchType;
+    private boolean backlit;
+
+    public Keyboard(int componentId, String componentName, int stock, double price, String typeOfComponent, String keySwitchType, boolean backlit) {
+        super(componentId, componentName ,stock, price, typeOfComponent);
+        this.keySwitchType = keySwitchType;
+        this.backlit = backlit;
+    }
+
+    public String getKeySwitchType() {
+        return keySwitchType;
+    }
+
+    public void setKeySwitchType(String keySwitchType) {
+        this.keySwitchType = keySwitchType;
+    }
+
+    public boolean isBacklit() {
+        return backlit;
+    }
+
+    public void setBacklit(boolean backlit) {
+        this.backlit = backlit;
+    }
+    
+    public String getComponentDetails(){
+   	 return super.getComponentDetails() + "\nKeySwitchType: " + this.keySwitchType + "\nBacklit: " + this.backlit;
+   }
+}
---
+++ code/iteration_1/src/BusinessLayer/CompositeProduct/Motherboard.java
@@ -0,0 +1,43 @@
+package compositeSoFar;
+
+public class Motherboard extends Component {
+
+    private String cpuSocketType;
+    private String series;
+    private String memoryStandard;
+
+    public Motherboard(int componentId, String componentName, int stock, double price, String typeOfComponent, String cpuSocketType, String series, String memoryStandard) {
+        super(componentId, componentName ,stock, price, typeOfComponent);
+        this.cpuSocketType = cpuSocketType;
+        this.series = series;
+        this.memoryStandard = memoryStandard;
+    }
+
+    public String getCpuSocketType() {
+        return cpuSocketType;
+    }
+
+    public void setCpuSocketType(String cpuSocketType) {
+        this.cpuSocketType = cpuSocketType;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getMemoryStandard() {
+        return memoryStandard;
+    }
+
+    public void setMemoryStandard(String memoryStandard) {
+        this.memoryStandard = memoryStandard;
+    }
+    
+    public String getComponentDetails(){
+   	 return super.getComponentDetails() + "\nCPU Socket Type: " + this.cpuSocketType + "\nSeries" + this.series + "\nMemoryStandard: " + this.memoryStandard;
+   }
+}
