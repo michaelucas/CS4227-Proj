bbe4950008456cf8daffe87d70ee64ea9ef09f87
Conor Duggan <13127004@studentmail.ul.ie>
2016-10-22 21:26:38 +0100

    Adding Comments

--- code/iteration_1/src/BusinessLayer/ProductList.java
+++ code/iteration_1/src/BusinessLayer/ProductList.java
@@ -22,10 +22,14 @@ public class ProductList {
 		String [] listOfComponentOptions = new String[] {"CPU", "GPU", "Keyboard", "MemoryDrive", "Monitor", "Motherboard", "Mouse", "RAM"};
 		ComputerSystem computerSystem = new ComputerSystem(13141252, "AlienWare Laptop", 6, "ComputerSystem", 1200.66, "Windows" );
 		
+		// Loop for each component type in listOfComponentOptions array
 		for(String index : listOfComponentOptions) {
+			// Initialise an ArrayList to be filled with component type stored in index
 			ArrayList<Component> listOfComponentTypeOptions = new ArrayList<Component>();
+			// Ask for an ArrayList of all products of index component type
 			listOfComponentTypeOptions = DataControl.getComponentTypeList(index);
 			
+			//Print out the list of components of index type
 			ProductListUI.printOutList(listOfComponentTypeOptions);
 			int userChoice = ProductListUI.readUserInput();
 			computerSystem.addComponent(listOfComponentTypeOptions.get(userChoice));
