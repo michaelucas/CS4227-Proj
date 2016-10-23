8f72f66f3c99d6cbf5e438ee3ccde2612b9456a2
michaelucas <13112732@studentmail.ul.ie>
2016-10-23 19:11:40 +0100

    Update AddComponentUI.java

--- code/iteration_1/src/UserInterfaceLayer/AddComponentUI.java
+++ code/iteration_1/src/UserInterfaceLayer/AddComponentUI.java
@@ -6,53 +6,6 @@ import java.util.Scanner;
 
 public class AddComponentUI {
 	
-	Scanner in = new Scanner(System.in);
-	DataControl d = new DataControl();
 	
-	public void addCPUComponent(String type) throws FileNotFoundException{
-		
-		System.out.println("String componentName, int stock, double price, String typeOfComponent, String series, String CPUSocketType");
-		d.writeNewComponentToFile(in.nextLine());	
-	}
-	
-	public void addGPUComponent(String type) throws FileNotFoundException{
-		System.out.println("String componentName, int stock, double price, String typeOfComponent, String memory, int displayPorts");
-		d.writeNewComponentToFile(in.nextLine());
-		
-	}
-
-	public void addKeyboardComponent(String type) throws FileNotFoundException{
-		System.out.println("String componentName, int stock, double price, String typeOfComponent, String keySwitchType, boolean backlit");
-		d.writeNewComponentToFile(in.nextLine());
-	
-	}
-
-	public void addMemoryDriveComponent(String type) throws FileNotFoundException{
-		System.out.println("String componentName, int stock, double price, String typeOfComponent, int driveCapacity, boolean SSD");
-		d.writeNewComponentToFile(in.nextLine());
-	}
-	
-	public void addMonitorComponent(String type) throws FileNotFoundException{
-		System.out.println("String componentName, int stock, double price, String typeOfComponent, String screenResolution, boolean curved, boolean is3d");
-		d.writeNewComponentToFile(in.nextLine());
-	}
-	
-	public void addMotherboardComponent(String type) throws FileNotFoundException{
-		System.out.println(" String componentName, int stock, double price, String typeOfComponent, String cpuSocketType, String series, String memoryStandard");
-		d.writeNewComponentToFile(in.nextLine());
-		
-	}
-	
-	public void addMouseComponent(String type) throws FileNotFoundException{
-		System.out.println("String componentName, int stock, String typeOfComponent, int dpi, boolean programmableButtons, boolean dpiSwitching");
-		d.writeNewComponentToFile(in.nextLine());
-		
-	}
-	
-	public void addRAMComponent(String type) throws FileNotFoundException{
-		System.out.println("String componentName, int stock, String typeOfComponent, int gigaBytes, String multiChannel");
-		d.writeNewComponentToFile(in.nextLine());
-		
-	}
 
 }
