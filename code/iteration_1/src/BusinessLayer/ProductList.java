90b90da2ec3efdd40fe7538a484c2085f2676557
michaelucas <13112732@studentmail.ul.ie>
2016-10-24 22:51:37 +0100

    Update ProductList.java

--- code/iteration_1/src/BusinessLayer/ProductList.java
+++ code/iteration_1/src/BusinessLayer/ProductList.java
@@ -45,6 +45,7 @@ public class ProductList {
 
 			SummaryUI.printOutSummary(computerSystem.getSummary());
 			summaryConfirmToContinue = SummaryUI.checkToContinue();
+			ReceiptUI aReceipt = new ReceiptUI(computerSystem);
 
 		}
 	}
