f71bf961278cd78bff6d19880f40d7fac1578972
michaelucas <13112732@studentmail.ul.ie>
2016-10-24 13:41:37 +0100

    Update DataControl.java

--- code/iteration_1/src/DataLayer/DataControl.java
+++ code/iteration_1/src/DataLayer/DataControl.java
@@ -2,10 +2,75 @@ package DataLayer;
 
 import java.util.ArrayList;
 
-public class data {
+public class DataControl {
 	
-	data() {
+	public final static String aComponentFile = "ComponentList.txt";
+	
+public static ArrayList<String> getUniqueComponentTypes() throws FileNotFoundException {
 		
+		File componentFile = new File(aComponentFile);
+		FactoryDesignPattern component = new FactoryDesignPattern();
+		ArrayList<Component> componentList = new ArrayList<Component>();
+		Scanner reader = new Scanner(componentFile);
+		while(reader.hasNextLine()){
+			String LineFromFile = reader.nextLine();
+			String[] pieceOfLine = LineFromFile.split(",");
+			
+			switch(pieceOfLine[4]){
+			case "CPU":
+				CPU cpuComponent = component.getCPU(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], pieceOfLine[5], 
+						pieceOfLine[6]);
+				componentList.add(cpuComponent);
+				break;
+			case "GPU":
+				GPU gpuComponent = component.getGPU(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]),Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], pieceOfLine[5], 
+						Integer.parseInt(pieceOfLine[6]));
+				componentList.add(gpuComponent);
+				break;
+			case "Keyboard":
+				Keyboard keyboardComponent = component.getKeyboard(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], pieceOfLine[5], 
+						Boolean.parseBoolean(pieceOfLine[6]));
+				componentList.add(keyboardComponent);
+				break;
+			case "MemoryDrive":
+				MemoryDrive memoryComponent = component.getMemoryDrive(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], Integer.parseInt(pieceOfLine[5]), 
+						Boolean.parseBoolean(pieceOfLine[6]));
+				componentList.add(memoryComponent);
+				break;
+			case "Monitor":
+				Monitor monitorComponent = component.getMonitor(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], pieceOfLine[5], 
+						Boolean.parseBoolean(pieceOfLine[6]), Boolean.parseBoolean(pieceOfLine[7]));
+				componentList.add(monitorComponent);
+				break;
+			case "Motherboard":
+				Motherboard motherboardComponent = component.getMotherboard(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], pieceOfLine[5],
+						pieceOfLine[6], pieceOfLine[7]);
+				componentList.add(motherboardComponent);
+				break;
+			case "Mouse":
+				Mouse mouseComponent = component.getMouse(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], Integer.parseInt(pieceOfLine[5]), 
+						Boolean.parseBoolean(pieceOfLine[6]), Boolean.parseBoolean(pieceOfLine[7]));
+				componentList.add(mouseComponent);
+				break;
+			case "RAM":
+				RAM ramComponent = component.getRAM(Integer.parseInt(pieceOfLine[0]), pieceOfLine[1], Integer.parseInt(pieceOfLine[2]), Double.parseDouble(pieceOfLine[3]), pieceOfLine[4], Integer.parseInt(pieceOfLine[5]), 
+						pieceOfLine[6]);
+				componentList.add(ramComponent);
+				break;
+			}
+			
+		}
+		reader.close();
+		ArrayList<String> uniqueComponentArrayList = new ArrayList<String>();
+		
+		for (int i = 0; i < componentList.size(); i++) {
+			if (!(uniqueComponentArrayList.contains(componentList.get(i).getTypeOfComponent())))
+				uniqueComponentArrayList.add(componentList.get(i).getTypeOfComponent());
+		}
+		
+		return uniqueComponentArrayList;
+	}
+	
 	}
 	
 	public static ArrayList<Component> getComponentTypeList(String componentType) {
