1e887496c065e5bfe7eb7f2d90a548106bb4a20e
conorjduggan-ul <13127004@studentmail.ul.ie>
2016-10-25 19:19:48 +0100

    Updating datacontrol component filepath

--- code/iteration_2/src/DataLayer/DataControl.java
+++ code/iteration_2/src/DataLayer/DataControl.java
@@ -13,7 +13,7 @@ import java.util.*;
 
 public class DataControl {
 	
-	public final static String componentFileName = "code/iteration_1/ComponentList.txt";
+	public final static String componentFileName = "code/iteration_2/ComponentList.txt";
 	
 	public static ArrayList<String> getUniqueComponentTypes() throws FileNotFoundException {
 		
