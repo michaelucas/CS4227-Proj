e2a6c0730ea35aa49c97f97c212ceb4faf2650a0
Cian Prendergast <cianp94@gmail.com>
2016-11-05 22:12:50 +0000

    Adding option to skip

--- code/iteration_2/src/UserInterfaceLayer/ProductListUI.java
+++ code/iteration_2/src/UserInterfaceLayer/ProductListUI.java
@@ -23,8 +23,9 @@ public class ProductListUI {
 		
 		System.out.println("\n\nPlease choose a component for your computer:\n");
 		for (int i = 0; i < listArray.size(); i++) {
-			System.out.println("Choice: " + (i + 1) + "\t" + " " + listArray.get(i).getComponentDetails() + "\n\nPress 0 to skip component.\nPress -1 to undo selection.");
+			System.out.println("Choice: " + (i + 1) + "\t" + " " + listArray.get(i).getComponentDetails() +"\n\n");
 		}
+		System.out.print("Press 0 to skip component.\nPress -1 to undo selection.\n");
 	}
 	
 	public static int readUserInput() {
