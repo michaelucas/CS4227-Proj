eedfd9b57cbc1505f22ef09c363503e5df40a325
danieloc <daniel.o.c.03@gmail.com>
2016-11-09 21:50:16 +0000

    Fix not being able to remove products

--- code/iteration_2/src/UserInterfaceLayer/AdminUI.java
+++ code/iteration_2/src/UserInterfaceLayer/AdminUI.java
@@ -8,10 +8,10 @@ import BusinessLayer.AdminProductDisplay;
 import BusinessLayer.AdminProductList;
 
 public class AdminUI {
-	
+
 	private AdminProductList pl;
 	private AdminProductDisplay display;
-	
+
 	public AdminUI() throws IOException {
 		pl = new AdminProductList();
 		display = new AdminProductDisplay();
@@ -19,15 +19,15 @@ public class AdminUI {
 	}
 
 	public void display() throws IOException{
-		
+
 		//v2
 		int choice = 1;
 		while (choice != 0){
-			
+
 			System.out.println(display.getAllDisplay());
 			System.out.println("\n\nWould you like to : \n1. Add a new Product\n2. Remove a Product\n0. Exit");
 			choice = display.readUserInput();
-			
+
 			if(choice == 1){
 				addComponent(pl);
 			}
@@ -36,23 +36,29 @@ public class AdminUI {
 			}
 		}
 	}
-	
+
 	public void addComponent(AdminProductList pl) throws FileNotFoundException{
 		Scanner in = new Scanner(System.in);
 		System.out.println("What type of product do you want to add (GPU,CPU etc.)?");
 		String answer = in.nextLine();
 		String componentToAdd = null;
-		
+
 		try {
 			System.out.println(display.addComponent(answer));
 			if(!("Invalid Input".equals(display.addComponent(answer))))
 				componentToAdd = in.nextLine();
-				System.out.println("How many are in Stock?");
-			    int stock = Integer.parseInt(in.nextLine());
-				pl.addToFile(componentToAdd, stock);
+			System.out.println("How many are in Stock?");
+			int stock = Integer.parseInt(in.nextLine());
+			pl.addToFile(componentToAdd, stock);
 
 		} catch (FileNotFoundException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public void removeComponent(AdminProductList pl) throws IOException{
+		System.out.println("Type the number of the product you want to remove");
+		int choice = display.readUserInput();
+		pl.removeComponent(choice);
 	}
 }
