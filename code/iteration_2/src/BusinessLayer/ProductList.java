adab76c49456cf56e4b9ba885a167399b5445557
conorjduggan-ul <13127004@studentmail.ul.ie>
2016-11-10 20:28:18 +0000

    Removing unused import ProductList

--- code/iteration_2/src/BusinessLayer/ProductList.java
+++ code/iteration_2/src/BusinessLayer/ProductList.java
@@ -13,7 +13,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import ArchitecturalLayer.ContextObject;
 import ArchitecturalLayer.Dispatcher;
