27a61507c90f3e80a5097270a356bd846f7518b0
Cian Prendergast <cianp94@gmail.com>
2016-11-12 14:05:07 +0000

    Update and rename Stock.java to Invoker.java

--- code/iteration_2/src/BusinessLayer/CommandProduct/Invoker.java
+++ code/iteration_2/src/BusinessLayer/CommandProduct/Invoker.java
@@ -3,12 +3,11 @@ package BusinessLayer.CommandProduct;
 import java.util.ArrayList;
 import java.util.List;
 
-   public class Stock {
-   private List<Order> orderList = new ArrayList<>(); 
+   public class Invoker {
+   private List<Order> orderList = new ArrayList<Order>(); 
 
    public void takeOrder(Order order){
       orderList.add(order);
-      order.execute();
    }
 
    public void placeOrders(){
