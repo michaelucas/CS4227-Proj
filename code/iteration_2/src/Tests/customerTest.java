d80e79de04b7aeceec3efb569d8c9e1a60944c41
danieloc <daniel.o.c.03@gmail.com>
2016-11-12 16:12:38 +0000

    Add Juit test for Vistior DP
    Adds junit test for visitor DP

--- code/iteration_2/src/Tests/customerTest.java
+++ code/iteration_2/src/Tests/customerTest.java
@@ -4,10 +4,14 @@ import BusinessLayer.CompositeProduct.CPU;
 import BusinessLayer.CompositeProduct.ComputerSystem;
 import BusinessLayer.CompositeProduct.GPU;
 import BusinessLayer.DecoratorReceipt.CreateReceipt;
+import BusinessLayer.VisitorShipping.ShippingVisitor;
+import BusinessLayer.VisitorShipping.VisitableElement;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
@@ -17,17 +21,17 @@ public class customerTest {
 	int cpuId = (int) new Date().getTime();
 	int gpuId = (int) new Date().getTime();
 	ComputerSystem testSystem = new ComputerSystem(compSysId, "TestSystem", "ComputerSystem" , "Windows", 123.00);
-	CPU testCPU = new CPU(cpuId, "testCPU", 120.00, "CPU","Series 1" , "12Pin", 0.111);
-	GPU testGPU = new GPU(gpuId, "testGPU", 120.00, "GPU", "4gb", 4, 12);
-	
+	CPU testCPU = new CPU(cpuId, "testCPU", 120.00, "CPU","Series 1" , "12Pin", 100000.111);
+	GPU testGPU = new GPU(gpuId, "testGPU", 20.00, "GPU", "4gb", 4, 12);
+
 	@Test
 	public void testComposite() {
 		testSystem.addComponent(testCPU);
 		testSystem.addComponent(testGPU);
 		double compSysPrice = testSystem.getPrice();
-		assertEquals(240.00, compSysPrice, 0);
+		assertEquals(140.00, compSysPrice, 0);
 	}
-	
+
 	@Test
 	public void testReceipt() {
 		testSystem.addComponent(testCPU);
@@ -35,8 +39,22 @@ public class customerTest {
 		CreateReceipt receipt1 = new CreateReceipt(testSystem);
 		System.out.println(receipt1.getReceipt());
 		String expectOutput = "CPU" + "\t\u20ac" + "120.0" + "\t" + "testCPU" + "\n";
-		String expectOutput2 = "GPU" + "\t\u20ac" + "120.0" + "\t" + "testGPU" + "\n";
+		String expectOutput2 = "GPU" + "\t\u20ac" + "20.0" + "\t" + "testGPU" + "\n";
 		assertThat(receipt1.getReceipt(), CoreMatchers.containsString(expectOutput));
 		assertThat(receipt1.getReceipt(), CoreMatchers.containsString(expectOutput2));
+	}
+
+	@Test
+	public void testVisitor() {
+		List<VisitableElement> cartItems = new ArrayList<>();;
+		cartItems.add(testCPU);
+		cartItems.add(testGPU);
+		ShippingVisitor shippingVisitor = new ShippingVisitor();
+
+		for (VisitableElement item: cartItems)
+			item.accept(shippingVisitor);
+
+		double shippingCostTotal = shippingVisitor.getTotalShipping();
+		assertEquals(36, shippingCostTotal, 0.0);
 	}
 }
