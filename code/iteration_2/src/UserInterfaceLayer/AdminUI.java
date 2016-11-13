a505aa873c47fc509d84b045e6f787bd71b92ed7
DarrenStack <13129783@studentmail.ul.ie>
2016-11-13 03:26:43 +0000

    Updated Observer references

--- code/iteration_2/src/UserInterfaceLayer/AdminUI.java
+++ code/iteration_2/src/UserInterfaceLayer/AdminUI.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import BusinessLayer.AdminProductDisplay;
-import BusinessLayer.AdminProductList;
+import BusinessLayer.ObserverProduct.AdminProductDisplay;
+import BusinessLayer.ObserverProduct.AdminProductList;
 
 public class AdminUI {
 
